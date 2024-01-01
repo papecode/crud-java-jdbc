d0073578806177daaeb36577713955ab516c9f82
papecode <papesaliouwade500@gmail.com>
2024-01-01 15:01:25 +0000

    feat: add list of all users

--- src/sn/jdbc/repository/implement/MySqlUserRepository.java
+++ src/sn/jdbc/repository/implement/MySqlUserRepository.java
@@ -3,6 +3,7 @@ package sn.jdbc.repository.implement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import sn.jdbc.DBManager;
@@ -52,8 +53,26 @@ public class MySqlUserRepository implements IRepository<User> {
 
 	@Override
 	public List<User> list() throws RepositoryException {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> users = new ArrayList<>();
+		
+		try (Connection connection = DBManager.getConnection()) {
+			String query = "Select * From T_Users";
+			PreparedStatement ps = connection.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				int identifiant = rs.getInt("id");
+				String login = rs.getString("login");
+				String password = rs.getString("password");
+				
+				User user = new User (identifiant, login, password);
+				users.add(user);
+			}
+		} catch (Exception e) {
+			throw new RepositoryException(e.getMessage());
+		}
+		return users;
+		
 	}
 
 	@Override
--- src/sn/jdbc/runtime/TestConnection.java
+++ src/sn/jdbc/runtime/TestConnection.java
@@ -1,6 +1,8 @@
 package sn.jdbc.runtime; // Ctrl + shif + o pour gérer les imports
 
 
+import java.util.List;
+
 import sn.jdbc.exceptions.RepositoryException;
 import sn.jdbc.repository.IRepository;
 import sn.jdbc.repository.entities.User;
@@ -11,28 +13,46 @@ public class TestConnection {
 	public static void main(String[] args) {
 		
 		IRepository<User> repository = new MySqlUserRepository();
-		
-		  try {
-	            int userIdToRead = 1;
-	            User user = repository.read(userIdToRead);
-
-	            if (user != null) {
-	                System.out.println("User found:");
-	                System.out.println("ID: " + user.getId());
-	                System.out.println("Login: " + user.getLogin());
-	                System.out.println("Password: " + user.getPassword());
-	            } else {
-	                System.out.println("User not found with ID: " + userIdToRead);
-	            }
-	        } catch (RepositoryException e) {
-	            System.out.println("Error reading user: " + e.getMessage());
-	        }
-	    }	
+		try {
+			
+			 List<User> userList = repository.list();
+			
+		if (!userList.isEmpty()) {
+            System.out.println("User list:");
+            for (User user : userList) {
+                System.out.println("ID: " + user.getId());
+                System.out.println("Login: " + user.getLogin());
+                System.out.println("Password: " + user.getPassword());
+                System.out.println("--------");
+            }
+        } else {
+            System.out.println("No users found in the database.");
+        }
+    } catch (RepositoryException e) {
+        System.out.println("Error retrieving user list: " + e.getMessage());
+    }
+}
+			
 		
 		
 }
 	
-		
+	 /*try {
+         int userIdToRead = 1;
+         User user = repository.read(userIdToRead);
+
+         if (user != null) {
+             System.out.println("User found:");
+             System.out.println("ID: " + user.getId());
+             System.out.println("Login: " + user.getLogin());
+             System.out.println("Password: " + user.getPassword());
+         } else {
+             System.out.println("User not found with ID: " + userIdToRead);
+         }
+     } catch (RepositoryException e) {
+         System.out.println("Error reading user: " + e.getMessage());
+     }
+ }
 		
 		
 		/*try {
